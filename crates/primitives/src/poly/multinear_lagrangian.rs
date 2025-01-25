91b0fff9015825b1f96522e45061d6fc0e2038ae
Thomas Coratger <thomas.coratger@gmail.com>
2025-01-25 23:16:08 +0100

    fix MultilinearLagrangianPolynomial random

--- crates/primitives/src/poly/multinear_lagrangian.rs
+++ crates/primitives/src/poly/multinear_lagrangian.rs
@@ -50,8 +50,7 @@ impl MultilinearLagrangianPolynomial {
 
     /// Generates a random multilinear Lagrangian polynomial with the given number of coefficients.
     pub fn random(n: usize) -> Self {
-        (0..n).map(|i| BinaryField128b::new(i as u128)).collect()
-        // (0..n).map(|i| BinaryField128b::random()).collect()
+        (0..n).map(|_| BinaryField128b::random()).collect()
     }
 
     /// Creates a new [`MultilinearLagrangianPolynomial`] with the given coefficients.
